564e58d5fccc81a3ddc5de69bcbddc91eaa7db53
Richard Collins <richardejcollins@gmail.com>
2026-01-25 21:00:08 -0800

    Add files for project template
    Summary:
    
    Test Plan:

---
+++ apps/web/src/testRoutes.tsx
@@ -0,0 +1,7 @@
+import { RouteObject } from 'react-router-dom';
+
+function ThrowError(): never {
+	throw new Error('Test error');
+}
+
+export const testRoutes: RouteObject[] = [{ path: '/test-error', element: <ThrowError /> }];
