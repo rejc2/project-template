b56ae0d70363c0e26d3e2da82c97001447dbddae
Richard Collins <richardejcollins@gmail.com>
2026-01-30 20:29:29 -0800

    Sort imports

--- prettier.config.js
+++ prettier.config.js
@@ -3,6 +3,12 @@ export default {
 	singleQuote: true,
 	tabWidth: 3,
 	useTabs: true,
+
+	plugins: ['@trivago/prettier-plugin-sort-imports'],
+	importOrder: ['<THIRD_PARTY_MODULES>', '^[./]'],
+	importOrderSeparation: false,
+	importOrderSortSpecifiers: true,
+
 	overrides: [
 		{
 			files: '**/package.json',
