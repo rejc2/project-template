80c2607e7f9743a2c1eb2d9b5445a204b4e3fa54
Richard Collins <richardejcollins@gmail.com>
2026-03-05 18:14:00 -0800

    Add theme

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,14 +1,19 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { routes } from './routes';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import { ErrorBoundary } from 'react-error-boundary';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { ErrorPage } from './pages/ErrorPage';
+import { routes } from './routes';
+import { theme } from './theme';
 
 const router = createBrowserRouter(routes);
 
 export function App() {
 	return (
-		<ErrorBoundary fallbackRender={({ error }) => <ErrorPage error={error} />}>
-			<RouterProvider router={router} />
-		</ErrorBoundary>
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<ErrorBoundary fallbackRender={({ error }) => <ErrorPage error={error} />}>
+				<RouterProvider router={router} />
+			</ErrorBoundary>
+		</ThemeProvider>
 	);
 }
---
+++ apps/web/src/theme.ts
@@ -0,0 +1,10 @@
+import { createTheme } from '@mui/material';
+
+export const theme = createTheme({
+	components: {},
+	palette: {},
+	shape: {},
+	typography: {
+		fontFamily: '"Open Sans", sans-serif',
+	},
+});
