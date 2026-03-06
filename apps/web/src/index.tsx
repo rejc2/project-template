2a3060b6ce27681d11f38048ed42e8196e346f8a
Richard Collins <richardejcollins@gmail.com>
2026-03-05 17:57:56 -0800

    Add Temporal polyfill

--- apps/web/src/index.tsx
+++ apps/web/src/index.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
 
+console.info(`Build date: ${__BUILD_DATE__}, commit: ${__COMMIT_HASH__}`);
+
 const rootElement = document.getElementById('root');
 if (!rootElement) {
 	throw new Error('Root element not found');
