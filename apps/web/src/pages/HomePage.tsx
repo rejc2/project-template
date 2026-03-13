9d13a56c86efd14f40fef6b5c46407c29e24cabc
Richard Collins <richardejcollins@gmail.com>
2026-03-13 12:36:42 -0700

    Separate books example route

--- apps/web/src/pages/HomePage.tsx
+++ apps/web/src/pages/HomePage.tsx
@@ -1,27 +1,9 @@
-import { Box, Button, List, ListItem, ListItemText, Typography } from '@mui/material';
-
-import { useBooks } from '@/api/books-example/books-example-api-hooks';
+import { Box, Typography } from '@mui/material';
 
 export function HomePage() {
-	const { data, fetchNextPage, hasNextPage, isFetchingNextPage } = useBooks({ first: 5 });
-
-	const books = data?.pages.flatMap((page) => page.books) ?? [];
-
 	return (
 		<Box sx={{ p: 3 }}>
 			<Typography variant="h1">Home</Typography>
-			<List>
-				{books.map((book) => (
-					<ListItem key={book.id}>
-						<ListItemText primary={book.title} secondary={book.authors.join(', ')} />
-					</ListItem>
-				))}
-			</List>
-			{hasNextPage && (
-				<Button onClick={() => fetchNextPage()} loading={isFetchingNextPage}>
-					Load more
-				</Button>
-			)}
 		</Box>
 	);
 }
